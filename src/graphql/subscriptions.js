385da9c9aca08de59d19378f0f1e89e0845f5935
Jacob Blazina <jacob@rivialsecurity.com>
2023-11-19 22:14:05 -0800

    Ran codegen graphql

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,456 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateBiography = /* GraphQL */ `
+  subscription OnCreateBiography(
+    $filter: ModelSubscriptionBiographyFilterInput
+    $owner: String
+  ) {
+    onCreateBiography(filter: $filter, owner: $owner) {
+      id
+      date
+      cadence
+      entry
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onUpdateBiography = /* GraphQL */ `
+  subscription OnUpdateBiography(
+    $filter: ModelSubscriptionBiographyFilterInput
+    $owner: String
+  ) {
+    onUpdateBiography(filter: $filter, owner: $owner) {
+      id
+      date
+      cadence
+      entry
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onDeleteBiography = /* GraphQL */ `
+  subscription OnDeleteBiography(
+    $filter: ModelSubscriptionBiographyFilterInput
+    $owner: String
+  ) {
+    onDeleteBiography(filter: $filter, owner: $owner) {
+      id
+      date
+      cadence
+      entry
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onCreateHealthReport = /* GraphQL */ `
+  subscription OnCreateHealthReport(
+    $filter: ModelSubscriptionHealthReportFilterInput
+    $owner: String
+  ) {
+    onCreateHealthReport(filter: $filter, owner: $owner) {
+      id
+      date
+      cadence
+      report
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onUpdateHealthReport = /* GraphQL */ `
+  subscription OnUpdateHealthReport(
+    $filter: ModelSubscriptionHealthReportFilterInput
+    $owner: String
+  ) {
+    onUpdateHealthReport(filter: $filter, owner: $owner) {
+      id
+      date
+      cadence
+      report
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onDeleteHealthReport = /* GraphQL */ `
+  subscription OnDeleteHealthReport(
+    $filter: ModelSubscriptionHealthReportFilterInput
+    $owner: String
+  ) {
+    onDeleteHealthReport(filter: $filter, owner: $owner) {
+      id
+      date
+      cadence
+      report
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onCreateJournalEntry = /* GraphQL */ `
+  subscription OnCreateJournalEntry(
+    $filter: ModelSubscriptionJournalEntryFilterInput
+    $owner: String
+  ) {
+    onCreateJournalEntry(filter: $filter, owner: $owner) {
+      id
+      date
+      cadence
+      entry
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onUpdateJournalEntry = /* GraphQL */ `
+  subscription OnUpdateJournalEntry(
+    $filter: ModelSubscriptionJournalEntryFilterInput
+    $owner: String
+  ) {
+    onUpdateJournalEntry(filter: $filter, owner: $owner) {
+      id
+      date
+      cadence
+      entry
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onDeleteJournalEntry = /* GraphQL */ `
+  subscription OnDeleteJournalEntry(
+    $filter: ModelSubscriptionJournalEntryFilterInput
+    $owner: String
+  ) {
+    onDeleteJournalEntry(filter: $filter, owner: $owner) {
+      id
+      date
+      cadence
+      entry
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onCreateNotification = /* GraphQL */ `
+  subscription OnCreateNotification(
+    $filter: ModelSubscriptionNotificationFilterInput
+    $owner: String
+  ) {
+    onCreateNotification(filter: $filter, owner: $owner) {
+      id
+      title
+      content
+      readDate
+      type
+      userID
+      isUnread
+      owner
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateNotification = /* GraphQL */ `
+  subscription OnUpdateNotification(
+    $filter: ModelSubscriptionNotificationFilterInput
+    $owner: String
+  ) {
+    onUpdateNotification(filter: $filter, owner: $owner) {
+      id
+      title
+      content
+      readDate
+      type
+      userID
+      isUnread
+      owner
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteNotification = /* GraphQL */ `
+  subscription OnDeleteNotification(
+    $filter: ModelSubscriptionNotificationFilterInput
+    $owner: String
+  ) {
+    onDeleteNotification(filter: $filter, owner: $owner) {
+      id
+      title
+      content
+      readDate
+      type
+      userID
+      isUnread
+      owner
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
+export const onCreateSubscriptionPlan = /* GraphQL */ `
+  subscription OnCreateSubscriptionPlan(
+    $filter: ModelSubscriptionSubscriptionPlanFilterInput
+    $owner: String
+  ) {
+    onCreateSubscriptionPlan(filter: $filter, owner: $owner) {
+      id
+      subscriptionTier
+      status
+      squareSubscriptionID
+      owner
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateSubscriptionPlan = /* GraphQL */ `
+  subscription OnUpdateSubscriptionPlan(
+    $filter: ModelSubscriptionSubscriptionPlanFilterInput
+    $owner: String
+  ) {
+    onUpdateSubscriptionPlan(filter: $filter, owner: $owner) {
+      id
+      subscriptionTier
+      status
+      squareSubscriptionID
+      owner
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteSubscriptionPlan = /* GraphQL */ `
+  subscription OnDeleteSubscriptionPlan(
+    $filter: ModelSubscriptionSubscriptionPlanFilterInput
+    $owner: String
+  ) {
+    onDeleteSubscriptionPlan(filter: $filter, owner: $owner) {
+      id
+      subscriptionTier
+      status
+      squareSubscriptionID
+      owner
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
+export const onCreateThought = /* GraphQL */ `
+  subscription OnCreateThought(
+    $filter: ModelSubscriptionThoughtFilterInput
+    $owner: String
+  ) {
+    onCreateThought(filter: $filter, owner: $owner) {
+      id
+      input
+      output
+      extract
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onUpdateThought = /* GraphQL */ `
+  subscription OnUpdateThought(
+    $filter: ModelSubscriptionThoughtFilterInput
+    $owner: String
+  ) {
+    onUpdateThought(filter: $filter, owner: $owner) {
+      id
+      input
+      output
+      extract
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onDeleteThought = /* GraphQL */ `
+  subscription OnDeleteThought(
+    $filter: ModelSubscriptionThoughtFilterInput
+    $owner: String
+  ) {
+    onDeleteThought(filter: $filter, owner: $owner) {
+      id
+      input
+      output
+      extract
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser(
+    $filter: ModelSubscriptionUserFilterInput
+    $owner: String
+  ) {
+    onCreateUser(filter: $filter, owner: $owner) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      Notifications {
+        nextToken
+        startedAt
+        __typename
+      }
+      profileImage
+      cognitoSub
+      owner
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser(
+    $filter: ModelSubscriptionUserFilterInput
+    $owner: String
+  ) {
+    onUpdateUser(filter: $filter, owner: $owner) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      Notifications {
+        nextToken
+        startedAt
+        __typename
+      }
+      profileImage
+      cognitoSub
+      owner
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser(
+    $filter: ModelSubscriptionUserFilterInput
+    $owner: String
+  ) {
+    onDeleteUser(filter: $filter, owner: $owner) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      Notifications {
+        nextToken
+        startedAt
+        __typename
+      }
+      profileImage
+      cognitoSub
+      owner
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
