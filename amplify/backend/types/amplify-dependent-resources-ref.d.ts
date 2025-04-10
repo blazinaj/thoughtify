57840d1089923bfd19b5b4f503c389933df6aa63
Jacob Blazina <blazinaj@hotmail.com>
2025-04-09 22:23:08 -0700

    amplify: catching up staging change

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -19,7 +19,7 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
-    "api": {
+    "handleCompletion": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
