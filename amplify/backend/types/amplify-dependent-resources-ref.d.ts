34add77c14a62b50d000146f68acc657a532eb49
Jacob Blazina <blazinaj@hotmail.com>
2025-04-16 10:29:32 -0700

    schema: pull

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -39,5 +39,11 @@ export type AmplifyDependentResourcesAttributes = {
       "language": "string",
       "region": "string"
     }
+  },
+  "storage": {
+    "attachments": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
